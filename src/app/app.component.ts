2904ba9af6f9069d059f58e8cc1726c19ff22e2b
mei li <mei90s.lee@gmail.com>
2018-09-30 14:18:35 +0800

    fix css build error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  //styleUrls:['./app.component.css']
+  styles:[require('./app.component.css').toString()]
 })
 export class AppComponent {
   title = 'angular-seed-project';
