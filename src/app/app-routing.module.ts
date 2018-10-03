0955630b9cc4b4bd2fa335255cce5947622ba51b
mei li <mei90s.lee@gmail.com>
2018-10-04 00:03:17 +0800

    angular preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+//import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AppCustomPreloader } from './app.custom.preloader';
 
 const routes: Routes = [
-    { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule' },
+    {
+        path: 'dashboard',
+        loadChildren: './modules/dashboard/dashboard.module#DashboardModule',
+        data: { preload: true }
+    },
     { path: 'settings', loadChildren: './modules/settings/settings.module#SettingsModule' },
     { path: 'reports', loadChildren: './modules/reports/reports.module#ReportsModule' },
 
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })],
-    exports: [RouterModule]
+    // imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })],
+    imports: [RouterModule.forRoot(routes, { preloadingStrategy: AppCustomPreloader, useHash: true })],
+    exports: [RouterModule],
+    providers: [AppCustomPreloader]
 })
 export class AppRoutingModule { }
