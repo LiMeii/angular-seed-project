81cea528cb6aa47993ba57005a083428fc6d85fa
mei li <mei90s.lee@gmail.com>
2018-10-08 17:43:51 +0800

    add files for what's in bunlde file

--- webpack/webpack.bundle.js
+++ webpack/webpack.bundle.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        'a': './src/app/bundle/a.js',
+        'a': './src/app/bundle/a.js'
     },
     performance: {
         hints: false
