668afebcd91b07c0131b2b1afbd29b106e0dbc5a
mei li <mei90s.lee@gmail.com>
2019-02-12 17:40:43 +0800

    child to parent component sharing data method

--- src/app/modules/data-sharing/data-sharing-routing.module.ts
+++ src/app/modules/data-sharing/data-sharing-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 import { ParentComponent } from './parent-to-child/parent.component';
+import { VoteParentComponent } from './child-to-parent/output/vote-parent.component';
+import { TimerParentComponent } from './child-to-parent/viewchild/timer-parent.component';
 
 const dataSharingRoute: Routes = [
     { path: '', component: ParentComponent },
     { path: 'parentToChild', component: ParentComponent },
+    { path: 'vote', component: VoteParentComponent },
+    { path: 'timer', component: TimerParentComponent }
 ];
 
 @NgModule({
--- src/app/modules/data-sharing/data-sharing.module.ts
+++ src/app/modules/data-sharing/data-sharing.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DataSharingRoutingModule } from './data-sharing-routing.module';
 import { ParentComponent } from './parent-to-child/parent.component';
 import { ChildComponent } from './parent-to-child/child.component';
+import { VoteParentComponent } from './child-to-parent/output/vote-parent.component';
+import { voteChildComponent } from './child-to-parent/output/vote-child.component';
+import { TimerParentComponent } from './child-to-parent/viewchild/timer-parent.component';
+import { TimerChildComponent } from './child-to-parent/viewchild/timer-child.component';
 
 @NgModule({
     imports: [
@@ -11,7 +15,11 @@ import { ChildComponent } from './parent-to-child/child.component';
         CommonModule],
     declarations: [
         ParentComponent,
-        ChildComponent
+        ChildComponent,
+        VoteParentComponent,
+        voteChildComponent,
+        TimerParentComponent,
+        TimerChildComponent
     ]
 
 })
